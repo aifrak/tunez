b87a2bfc174f2972cb44c4ba441e92d5959767c5
aifrak <aifrak@users.noreply.github.com>
2025-12-11 22:24:09 +0100

    Complete `Chapter 4 - Generating APIs Without Writing Code`

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -1,5 +1,17 @@
 defmodule Tunez.Music.Album do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer,
+    extensions: [AshGraphql.Resource, AshJsonApi.Resource]
+
+  graphql do
+    type :album
+  end
+
+  json_api do
+    type "album"
+  end
 
   postgres do
     table "albums"
@@ -43,13 +55,18 @@ defmodule Tunez.Music.Album do
 
     attribute :name, :string do
       allow_nil? false
+      public? true
     end
 
     attribute :year_released, :integer do
       allow_nil? false
+      public? true
     end
 
-    attribute :cover_image_url, :string
+    attribute :cover_image_url, :string do
+      public? true
+    end
+
     create_timestamp :inserted_at
     update_timestamp :updated_at
   end
@@ -59,6 +76,8 @@ defmodule Tunez.Music.Album do
       allow_nil? false
     end
   end
+
+  def next_year, do: Date.utc_today().year + 1
 
   calculations do
     calculate :years_ago, :integer, expr(2025 - year_released)
@@ -72,6 +91,4 @@ defmodule Tunez.Music.Album do
     identity :unique_album_names_per_artist, [:name, :artist_id],
       message: "already exists for this artist"
   end
-
-  def next_year, do: Date.utc_today().year + 1
 end
--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -1,5 +1,27 @@
 defmodule Tunez.Music.Artist do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer,
+    extensions: [AshGraphql.Resource, AshJsonApi.Resource]
+
+  graphql do
+    type :artist
+
+    filterable_fields [
+      :album_count,
+      :cover_image_url,
+      :inserted_at,
+      :latest_album_year_released,
+      :updated_at
+    ]
+  end
+
+  json_api do
+    type "artist"
+    includes [:albums]
+    derive_filter? false
+  end
 
   postgres do
     table "artists"
@@ -8,6 +30,10 @@ defmodule Tunez.Music.Artist do
     custom_indexes do
       index "name gin_trgm_ops", name: "artists_name_gin_index", using: "GIN"
     end
+  end
+
+  resource do
+    description "A person or group of people that makes and releases music."
   end
 
   actions do
@@ -22,7 +48,10 @@ defmodule Tunez.Music.Artist do
     end
 
     read :search do
+      description "List Artists, optionally filtering by name."
+
       argument :query, :ci_string do
+        description "Return only artists with names including the given value."
         constraints allow_empty?: true
         default ""
       end
@@ -43,10 +72,13 @@ defmodule Tunez.Music.Artist do
       public? true
     end
 
-    attribute :biography, :string
+    attribute :biography, :string do
+      public? true
+    end
 
     attribute :previous_names, {:array, :string} do
       default []
+      public? true
     end
 
     create_timestamp :inserted_at, public?: true
@@ -56,6 +88,7 @@ defmodule Tunez.Music.Artist do
   relationships do
     has_many :albums, Tunez.Music.Album do
       sort year_released: :desc
+      public? true
     end
   end
 
